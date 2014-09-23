c8e1f98a9f364da5b54f61068da829723e2429d9
einhep <einhep@gmail.com>
2014-09-24 01:48:53 +0800

    change_application_css

---
+++ vendor/assets/javascripts/custom.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+//第三列title active切换
+function OnActive_recommond(){
+	$("#guanzhu_list").removeClass("active");
+	$("#recommond").addClass("active");
+}
+function OnActive_guanzhulist(){
+	$("#guanzhu_list").addClass("active");
+	$("#recommond").removeClass("active");
+}
